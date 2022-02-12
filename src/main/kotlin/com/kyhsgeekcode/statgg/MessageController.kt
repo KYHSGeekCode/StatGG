df8f6064e3944b52da2e4986b184aeef0f82d6f2
KYHSGeekCode <jourhyang123@gmail.com>
2022-02-12 21:09:26 +0900

    :tada: Begin project

---
+++ src/main/kotlin/com/kyhsgeekcode/statgg/MessageController.kt
@@ -0,0 +1,17 @@
+package com.kyhsgeekcode.statgg
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MessageController {
+
+    @GetMapping("/message")
+    fun index(): List<Message> = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!")
+    )
+}
+
+data class Message(val id: String?, val text: String)
---
+++ src/main/kotlin/com/kyhsgeekcode/statgg/StatggApplication.kt
@@ -0,0 +1,11 @@
+package com.kyhsgeekcode.statgg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StatggApplication
+
+fun main(args: Array<String>) {
+    runApplication<StatggApplication>(*args)
+}
